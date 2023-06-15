d883a89acc096aace12b1c7465fefb533af34800
jgmdev <jgmdev@gmail.com>
2023-06-15 19:06:04 -0400

    profiler: added support for luajit native profiler

--- plugins/profiler/README.md
+++ plugins/profiler/README.md
@@ -6,8 +6,8 @@ of the application. For more information view the [wikipedia] article.
 
 This plugin adds the ability to profile function calls while running Pragtical,
 becoming easier to investigate performance related issues and pinpoint what
-could be causing them. It integrates the [lua-profiler] which provides
-the functionality we need.
+could be causing them. It integrates the [lua-profiler] when running with
+PUC LUA or the more performant [luajit-profiler] when running with LuaJIT.
 
 ## Usage
 
@@ -18,10 +18,11 @@ the events that are causing any performance issues.
 
 ![command](https://user-images.githubusercontent.com/1702572/202113672-6ba593d9-03be-4462-9e82-e3339cf2722f.png)
 
-> **Note:** Starting the profiler will make the editor slower since it is
-> now accumulating metrics about every function call. Do not worry, this is
-> expected and shouldn't affect the end result, just be patience because
-> everything will be slower.
+> **Note:** Starting the profiler with the [lua-profiler] will make the editor
+> slower since it is now accumulating metrics about every function call. Do not
+> worry, this is expected and shouldn't affect the end result, just be patience
+> because everything will be slower. (performance impact is barely noticeable
+> when using the [luajit-profiler])
 
 There may be some situations when you would like to enable the profiler
 early on the startup process so we provided a configuration option for that.
@@ -41,8 +42,57 @@ as shown below:
 
 ![metrics](https://user-images.githubusercontent.com/1702572/202113736-ef8d550c-130e-4372-b66c-694ee5f4c5c0.png)
 
-You can send Pragtical developers the output of `profiler.log` so it can be
-more easily diagnosed what could be causing any issue.
+## LuaJIT Profiler
+
+When the editor is compiled with LuaJIT support a native more performant
+profiler developed by Mike Pall can be use. This profiler allows generating
+raw data that can be read by third party tools to generate more graphical
+visualizations like [FlameGraph], eg: `flamegraph profiler.log out.svg`.
+
+It also offers more control on the output data by using flags documented below.
+By default the highlevel profiler developed by Mike Pall is included and used
+by this plugin, if you disable the highlevel profiler a more basic dump will
+be generated.
+
+Support for displaying code `zones` with the highlevel profiler is available
+by using the `z` flag. To give regions of your code a certain name you can
+do the following:
+
+```lua
+local zone = require("plugins.profiler.zone")
+zone("ZoneName")
+  --- ... your lua code here
+  zone("ZoneNameTwo")
+    --- ... your lua code here
+  zone()
+  --- ... your lua code here
+zone()
+```
+
+### HighLevel JIT Profiler Flags
+
+* `f` - Stack dump: function name, otherwise module:line. Default mode.
+* `F` - Stack dump: ditto, but always prepend module.
+* `l` - Stack dump: module:line.
+* `<number>` - stack dump depth (callee < caller). Default: 1.
+* `-<number>` - Inverse stack dump depth (caller > callee).
+* `s` - Split stack dump after first stack level. Implies abs(depth) >= 2.
+* `p` - Show full path for module names.
+* `v` - Show VM states. Can be combined with stack dumps, e.g. vf or fv.
+* `z` - Show zones. Can be combined with stack dumps, e.g. zf or fz.
+* `r` - Show raw sample counts. Default: show percentages.
+* `a` - Annotate excerpts from source code files.
+* `A` - Annotate complete source code files.
+* `G` - Produce raw output suitable for graphical tools (e.g. flame graphs).
+* `m<number>` - Minimum sample percentage to be shown. Default: 3.
+* `i<number>` - Sampling interval in milliseconds. Default: 10.
+
+## Last Steps
+
+You can send Pragtical developers the output of `profiler.log` for easier
+diagnosing of performant related issues.
 
 [wikipedia]: https://en.wikipedia.org/wiki/Profiling_(computer_programming)
 [lua-profiler]: https://github.com/charlesmallah/lua-profiler
+[luajit-profiler]: https://github.com/LuaJIT/LuaJIT/blob/v2.1/doc/ext_profiler.html
+[FlameGraph]: https://github.com/brendangregg/FlameGraph
--- plugins/profiler/init.lua
+++ plugins/profiler/init.lua
@@ -4,7 +4,10 @@ local core = require "core"
 local common = require "core.common"
 local config = require "core.config"
 local command = require "core.command"
-local profiler = require "plugins.profiler.profiler"
+local profiler_lua = require "plugins.profiler.lua"
+local profiler_jit = LUAJIT and require("jit.profile")
+local profiler_jit_high = LUAJIT and require("plugins.profiler.jit")
+local profiler_data = {}
 
 --Keep track of profiler status.
 local RUNNING = false
@@ -16,6 +19,11 @@ local DEFAULT_LOG_PATH = USERDIR .. PATHSEP .. "profiler.log"
 config.plugins.profiler = common.merge({
   enable_on_startup = false,
   log_file = DEFAULT_LOG_PATH,
+  jit_profiler = true,
+  raw = false,
+  highlevel = true,
+  options = "f",
+  depth = 3,
   config_spec = {
     name = "Profiler",
     {
@@ -32,6 +40,44 @@ config.plugins.profiler = common.merge({
       type = "file",
       default = DEFAULT_LOG_PATH,
       filters = {"%.log$"}
+    },
+    {
+      label = "JIT Profiler",
+      description = "Use the LuaJIT profiler, applicable only with LuaJIT.",
+      path = "jit_profiler",
+      type = "toggle",
+      default = true
+    },
+    {
+      label = "JIT Profiler Raw",
+      description = "The generated output file can be read by graphical tools, eg: `flamegraph profiler.log > out.svg`",
+      path = "raw",
+      type = "toggle",
+      default = false
+    },
+    {
+      label = "JIT Profiler HighLevel",
+      description = "Use Mike Pall High Level Profiler.",
+      path = "highlevel",
+      type = "toggle",
+      default = true
+    },
+    {
+      label = "JIT Profiler HighLevel Options",
+      description = "Flags that control the profiler output, see README.md.",
+      path = "options",
+      type = "string",
+      default = "f"
+    },
+    {
+      label = "JIT Profiler Depth",
+      description = "Maximum calls depth to register.",
+      path = "depth",
+      type = "number",
+      default = 3,
+      min = -100,
+      max = 10,
+      step = 1
     }
   }
 }, config.plugins.profiler)
@@ -41,14 +87,119 @@ local Profiler = {}
 
 function Profiler.start()
   if RUNNING then return end
-  profiler.start()
+  if not LUAJIT or not config.plugins.profiler.jit_profiler then
+    profiler_lua.start()
+  elseif config.plugins.profiler.raw then
+    profiler_jit_high.start("G", config.plugins.profiler.log_file)
+  elseif config.plugins.profiler.highlevel then
+    profiler_jit_high.start(
+      config.plugins.profiler.depth .. config.plugins.profiler.options,
+      config.plugins.profiler.log_file
+    )
+  else
+    profiler_data = {}
+    profiler_jit.start("li1", function(th, samples, vmstate)
+      local sep = ""
+      if config.plugins.profiler.depth > 1 then
+        sep = " <- "
+      end
+      local path = profiler_jit.dumpstack(th, "pl"..sep, config.plugins.profiler.depth)
+      local dump = profiler_jit.dumpstack(th, "f"..sep, config.plugins.profiler.depth)
+      profiler_data[dump] = {
+        (profiler_data[dump] and profiler_data[dump][1] or 0) + samples,
+        path,
+        vmstate
+      }
+    end)
+  end
   RUNNING = true
+end
+
+---@param str string
+---@param text string
+---@param replacement string
+---@return string
+local function replace(str, text, replacement)
+  local s, e = str:find(text, 1, true)
+  while s do
+    local new_text = str:sub(1,s-1) .. replacement
+    local next_index = #new_text + 1
+    str = new_text .. str:sub(e+1)
+    s, e = str:find(text, next_index, true)
+  end
+  return str
 end
 
 function Profiler.stop()
   if RUNNING then
-    profiler.stop()
-    profiler.report(config.plugins.profiler.log_file)
+    if not LUAJIT or not config.plugins.profiler.jit_profiler then
+      profiler_lua.stop()
+      profiler_lua.report(config.plugins.profiler.log_file)
+    elseif config.plugins.profiler.raw or config.plugins.profiler.highlevel then
+      profiler_jit_high.stop()
+    else
+      profiler_jit.stop()
+      local log_file = io.open(config.plugins.profiler.log_file, "w+")
+      if log_file then
+        local sorted_data = {}
+        local sw, dw = 0, 0
+        for dump, data in pairs(profiler_data) do
+          if config.plugins.profiler.depth > 1 then
+            dump = string.gsub(dump, " <%- $", "")
+            data[2] = string.gsub(data[2], " <%- $", "")
+          end
+          data[2] = replace(data[2], DATADIR .. PATHSEP, "")
+          data[2] = replace(data[2], USERDIR .. PATHSEP, "")
+          sw = math.max(sw, string.len(tostring(data[1])))
+          dw = math.max(dw, #dump)
+          table.insert(sorted_data, {dump, data})
+        end
+        table.sort(sorted_data, function (a, b)
+          return a[2][1] > b[2][1]
+        end)
+        local state = {
+          N = "Compiled",
+          I = "Interpreted",
+          C = "C Code",
+          G = "Garbage Collector",
+          J = "JIT compiler"
+        }
+        log_file:write(
+          string.format(
+            "%-"..sw.."s | %-"..dw.."s | %s\n",
+            "#",
+            "Func",
+            "State"
+          )
+        )
+        local divider = string.rep("-", sw + 3 + dw + 3 + 17) .. "\n"
+        log_file:write(divider)
+        for _, data in ipairs(sorted_data) do
+          -- samples | dump | vmstate,
+          --         | locations
+          log_file:write(
+            string.format(
+              "%-"..sw.."s | %-"..dw.."s | %s\n",
+              data[2][1],
+              data[1],
+              state[data[2][3]]
+            )
+          )
+          for location in (data[2][2].." <- "):gmatch("(.-)".." <%- ") do
+            log_file:write(
+              string.format(
+                "%-"..sw.."s  -> %s\n",
+                "",
+                location
+              )
+            )
+          end
+          log_file:write(divider)
+        end
+        log_file:close()
+      end
+    end
+    profiler_data = {}
     RUNNING = false
   end
 end
@@ -86,8 +237,16 @@ command.add(nil, {
   ["profiler:toggle"] = function()
     if RUNNING then
       Profiler.stop()
-      core.log("Profiler: stopped")
-      core.root_view:open_doc(core.open_doc(config.plugins.profiler.log_file))
+      if not config.plugins.profiler.raw then
+        core.log("Profiler: stopped")
+        core.root_view:open_doc(core.open_doc(config.plugins.profiler.log_file))
+        -- in case the document was already open we reload it
+        core.add_thread(function ()
+          command.perform "doc:reload"
+        end)
+      else
+        core.log("Profiler: stopped and raw data saved to log file")
+      end
     else
       Profiler.start()
       core.log("Profiler: started")
