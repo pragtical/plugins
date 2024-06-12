e47ab4e1ffeb7f97cc17ad5fe350f811014330f6
jgmdev <jgmdev@gmail.com>
2024-06-12 02:42:20 -0400

    Improvements to language_ini
    * Better numbers matching
    * Support values separated by comments symbol, eg: value1;value2;value3
    * Added .conf extension
    * colorize equal-sign (=) as operator
    * Adjusted matching of inline comments to work better with the new
      changes.

--- plugins/language_ini.lua
+++ plugins/language_ini.lua
@@ -4,21 +4,36 @@ local syntax = require "core.syntax"
 
 syntax.add {
   name = "INI",
-  files = { "%.ini$", "%.inf$", "%.cfg$", "%.editorconfig$" },
+  files = { "%.ini$", "%.conf$", "%.inf$", "%.cfg$", "%.editorconfig$" },
   comment = ';',
   patterns = {
+    -- comments
     { pattern = ";.-\n", type = "comment" },
     { pattern = "#.-\n", type = "comment" },
+    -- sections
     { pattern = { "%[", "%]" }, type = "keyword" },
-
+    -- strings
     { pattern = { '"""', '"""', '\\' }, type = "string" },
     { pattern = { '"', '"', '\\' }, type = "string" },
     { pattern = { "'''", "'''" }, type = "string" },
     { pattern = { "'", "'" }, type = "string" },
-    { pattern = "[A-Za-z0-9_%.%-]+%s*%f[=]", type = "function" },
-    { pattern = "[%-+]?[0-9_]+%.[0-9_]+", type = "number" },
-    { pattern = "[%-+]?[0-9_]+", type = "number" },
-    { pattern = "[a-z]+", type = "symbol" },
+    -- keys
+    { pattern = "^[^=]+()=%s*", type = {"function", "operator"} },
+    -- prevent treating boolean values with ending space as non enclosed string
+    { pattern = "true%s+", type = "literal" },
+    { pattern = "false%s+", type = "literal" },
+    -- allows ini values that use comments symbol as value separator
+    -- when no spaces are used between the symbol eg: value1;value2;value3
+    { pattern = "[^%d%s][^%s]*[#;]%S*", type = "symbol" },
+    { pattern = "[^%s]+%d+[^%s]*[#;]%S*", type = "symbol" },
+    -- any non enclosed string value up to a starting comment
+    { pattern = "[^%-+%.'\"#;%d]+[^#;]+()%s[#;].*", type = {"symbol", "comment"} },
+    { pattern = "%a+%S*", type = "symbol" },
+    -- numbers values
+    { pattern = "[%-+]?%.?%d[%d_]*%.[%d_]+%f[%s%.]", type = "number" },
+    { pattern = "[%-+]?%.?%d[%d_]*%f[%s]", type = "number" },
+    -- any non enclosed string value
+    { pattern = "%a+", type = "symbol" },
   },
   symbols = {
     ["true"] = "literal",
