5ee58b8def83fc503482a91adf2a8f1f51838319
jgmdev <jgmdev@gmail.com>
2025-06-05 19:01:34 -0400

    centerdoc: added option to hide tabs on zen mode

--- plugins/centerdoc.lua
+++ plugins/centerdoc.lua
@@ -10,7 +10,8 @@ local DocView = require "core.docview"
 
 config.plugins.centerdoc = common.merge({
   enabled = true,
-  zen_mode = false
+  zen_mode = false,
+  zen_mode_hide_tabs = true
 }, config.plugins.centerdoc)
 
 local draw_line_gutter = DocView.draw_line_gutter
@@ -40,18 +41,27 @@ function DocView:get_gutter_width()
   end
 end
 
+---@type system.windowmode
+local previous_win_status
+---@type boolean
+local previous_treeview_status
+---@type boolean
+local previous_statusbar_status
+---@type boolean
+local previous_tabs_status
 
-local previous_win_status = system.get_window_mode(core.window)
-local previous_treeview_status = treeview.visible
-local previous_statusbar_status = core.status_view.visible
+local function save_previous_status()
+  previous_win_status = system.get_window_mode(core.window)
+  previous_treeview_status = treeview.visible
+  previous_statusbar_status = core.status_view.visible
+  previous_tabs_status = config.hide_tabs
+end
 
 local function toggle_zen_mode(enabled)
   config.plugins.centerdoc.zen_mode = enabled
 
   if config.plugins.centerdoc.zen_mode then
-    previous_win_status = system.get_window_mode(core.window)
-    previous_treeview_status = treeview.visible
-    previous_statusbar_status = core.status_view.visible
+    save_previous_status()
 
     config.plugins.centerdoc.enabled = true
     if previous_win_status ~= "fullscreen" then
@@ -59,6 +69,9 @@ local function toggle_zen_mode(enabled)
     end
     treeview.visible = false
     command.perform "status-bar:hide"
+    if config.plugins.centerdoc.zen_mode_hide_tabs then
+      config.hide_tabs = true
+    end
   else
     config.plugins.centerdoc.enabled = false
     if
@@ -70,10 +83,14 @@ local function toggle_zen_mode(enabled)
     end
     treeview.visible = previous_treeview_status
     core.status_view.visible = previous_statusbar_status
+    if config.plugins.centerdoc.zen_mode_hide_tabs then
+      config.hide_tabs = previous_tabs_status
+    end
   end
 end
 
 local on_startup = true
+save_previous_status()
 
 -- The config specification used by the settings gui
 config.plugins.centerdoc.config_spec = {
@@ -94,14 +111,24 @@ config.plugins.centerdoc.config_spec = {
     on_apply = function(enabled)
       if on_startup then
         core.add_thread(function()
-          previous_win_status = system.get_window_mode(core.window)
-          previous_treeview_status = treeview.visible
-          previous_statusbar_status = core.status_view.visible
+          save_previous_status()
           toggle_zen_mode(enabled)
         end)
         on_startup = false
       else
         toggle_zen_mode(enabled)
+      end
+    end
+  },
+  {
+    label = "Hide Tabs on Zen Mode",
+    description = "Creates a more inmmersive experience (requires pragtical >= v3.5.3).",
+    path = "zen_mode_hide_tabs",
+    type = "toggle",
+    default = true,
+    on_apply = function(enabled)
+      if config.plugins.centerdoc.zen_mode then
+        config.hide_tabs = enabled
       end
     end
   }
