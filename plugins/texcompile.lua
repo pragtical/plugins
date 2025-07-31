57060a4e9557ffd0737e9e6b9dc72a03deccd989
jgmdev <jgmdev@gmail.com>
2025-07-31 10:59:43 -0400

    Merge pdfview to texcompile and use common.open_in_system when available

--- plugins/texcompile.lua
+++ plugins/texcompile.lua
@@ -8,11 +8,11 @@ local keymap = require "core.keymap"
 
 -- This plugin requires the console plugin to work. It can be found at:
 --
--- https://github.com/lite-xl/console
+-- https://github.com/pragtical/console
 --
 -- Before using this plugin add in your user's config file something like:
 --
--- config.texcompile = {
+-- config.plugins.texcompile = {
 --   latex_command = "pdflatex",
 --   view_command = "evince",
 -- }
@@ -26,13 +26,36 @@ local keymap = require "core.keymap"
 -- On Windows, if the commands are not in your PATH, you may use the full path
 -- of the executable like, for example:
 --
--- config.texcompile = {
+-- config.plugins.texcompile = {
 --   latex_command = [[C:\miktex\miktex\bin\x64\pdflatex.exe]],
 --   view_command = [[C:\Program^ Files\SumatraPDF\SumatraPDF.exe]],
 -- }
 --
 -- Note that in the example we have used "^ " for spaces that appear in the path.
 -- It is required on Windows for path or file names that contains space characters.
+
+config.plugins.texcompile = common.merge({
+  enabled = true,
+  latex_command = "",
+  view_command = "",
+  -- The config specification used by the settings gui
+  config_spec = {
+    name = "Latex Compile",
+    {
+      label = "Latex Compiler Command",
+      description = "Name or path to the latex compiler command.",
+      path = "latex_command",
+      type = "string"
+    },
+    {
+      label = "PDF Viewer Command",
+      description = "Name or path to the pdf viewer command.",
+      path = "view_command",
+      type = "string"
+    }
+  }
+}, config.plugins.texcompile)
+
 
 command.add("core.docview!", {
   ["texcompile:tex-compile"] = function(dv)
@@ -41,22 +64,66 @@ command.add("core.docview!", {
     local texpath = common.dirname(dv:get_filename())
     local pdfname = texname:gsub("%.tex$", ".pdf")
 
-    -- LaTeX compiler as configured in config.texcompile
-    local texcmd = config.texcompile and config.texcompile.latex_command
-    local viewcmd = config.texcompile and config.texcompile.view_command
+    -- LaTeX compiler as configured in config.plugins.texcompile
+    local texcmd = config.plugins.texcompile and config.plugins.texcompile.latex_command
+    local viewcmd = config.plugins.texcompile and config.plugins.texcompile.view_command
 
-    if not texcmd then
+    if not texcmd or texcmd == "" then
       core.log("No LaTeX compiler provided in config.")
     else
       core.log("LaTeX compiler is %s, compiling %s", texcmd, texname)
 
       console.run {
-        command = string.format("%s %s && %s %s", texcmd, texname, viewcmd, pdfname),
+        command = string.format("%s %q", texcmd, texname),
         cwd = texpath,
-        on_complete = function() core.log("Tex compiling command terminated.") end
+        on_complete = function()
+          core.log("Tex compiling command terminated.")
+          if viewcmd and viewcmd ~= ""  then
+            system.exec(string.format("%q %q", viewcmd, pdfname))
+          elseif common.open_in_system then
+            common.open_in_system(pdfname)
+          else
+            core.log("No PDF viewer provided in config.")
+          end
+        end
       }
     end
   end,
+
+  ["texcompile:show-pdf-preview"] = function(av)
+    -- User's home directory
+    local homedir = ""
+
+    if PLATFORM == "Windows" then
+        homedir = os.getenv("USERPROFILE")
+    else
+        homedir = os.getenv("HOME")
+    end
+
+    -- The current (La)TeX file
+    local texfile = av:get_filename()
+    texfile = string.gsub(texfile, '~', homedir)
+    -- Construct the PDF file name out of the (La)Tex filename
+    local pdffile = "\"" .. string.gsub(texfile, ".tex", ".pdf") .. "\""
+    -- PDF viewer - is there any provided by the environment
+    local pdfcmd = config.plugins.texcompile and config.plugins.texcompile.view_command
+
+    core.log("Opening pdf preview for \"%s\"", texfile)
+
+    if pdfcmd and pdfcmd ~= "" then
+      system.exec(string.format("%q %q", pdfcmd, pdffile))
+    elseif common.open_in_system then
+      common.open_in_system(pdffile)
+    else
+      core.log("No PDF viewer provided in config.")
+    end
+
+   -- core.add_thread(function()
+   --   coroutine.yield(5)
+   --   os.remove(htmlfile)
+   -- end)
+  end
 })
 
 keymap.add { ["ctrl+shift+t"] = "texcompile:tex-compile" }
+keymap.add { ["ctrl+shift+v"] = "texcompile:show-pdf-preview" }
